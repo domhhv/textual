048f7d60a083eb7a4e622a0787317f33ecace552
Dominik Hryshaiev <domhryshaiev@gmail.com>
2025-09-21 15:11:41 +0200

    chore: configure eslint, prettier and code health ci

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,10 +1,10 @@
 const configuration = {
-	extends: ["@commitlint/config-conventional"],
-	rules: {
-		"type-case": [2, "always", "lower-case"],
-		"scope-case": [2, "always", "lower-case"],
-		"subject-case": [2, "always", ["lower-case", "pascal-case", "camel-case"]],
-	},
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', ['lower-case', 'pascal-case', 'camel-case']],
+    'type-case': [2, 'always', 'lower-case'],
+  },
 };
 
 export default configuration;
