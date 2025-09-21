e9ff6e06a91183c44f415a3f9e62994eba5e4fcb
Dominik Hryshaiev <domhryshaiev@gmail.com>
2025-09-21 15:13:10 +0200

    chore: configure eslint, prettier and code health ci (#2)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,149 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import perfectionist from 'eslint-plugin-perfectionist';
+import switchCase from 'eslint-plugin-switch-case';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
     ],
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:switch-case/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier'
+  ),
+  {
+    plugins: {
+      perfectionist,
+      'switch-case': switchCase,
+      'unused-imports': unusedImports,
+    },
+
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'arrow-body-style': ['error', 'always'],
+      curly: 'error',
+      'import/no-duplicates': 'error',
+      'import/order': 'off',
+      'no-undef': 'off',
+      'no-useless-rename': 'error',
+      'object-shorthand': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'switch-case/no-case-curly': 'off',
+
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
+
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', next: 'return', prev: '*' },
+        { blankLine: 'always', next: 'block-like', prev: '*' },
+        { blankLine: 'always', next: 'block', prev: '*' },
+        { blankLine: 'always', next: '*', prev: 'block-like' },
+        { blankLine: 'always', next: '*', prev: 'block' },
+      ],
+
+      'perfectionist/sort-exports': [
+        'error',
+        {
+          ignoreCase: true,
+          order: 'asc',
+          type: 'alphabetical',
+        },
+      ],
+
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          ignoreCase: true,
+          newlinesBetween: 'never',
+          order: 'asc',
+          tsconfigRootDir: '.',
+          type: 'alphabetical',
+          groups: [
+            'builtin',
+            { newlinesBetween: 'always' },
+            'external',
+            { newlinesBetween: 'always' },
+            'internal',
+            { newlinesBetween: 'always' },
+            'parent',
+            { newlinesBetween: 'always' },
+            ['index', 'sibling'],
+          ],
+        },
+      ],
+
+      'perfectionist/sort-jsx-props': [
+        'error',
+        {
+          type: 'line-length',
+        },
+      ],
+
+      'perfectionist/sort-named-imports': [
+        'error',
+        {
+          type: 'line-length',
+        },
+      ],
+
+      'perfectionist/sort-object-types': [
+        'error',
+        {
+          groups: ['unknown', 'method', 'multiline-member'],
+        },
+      ],
+
+      'perfectionist/sort-objects': [
+        'error',
+        {
+          groups: ['unknown', 'method', 'multiline-member'],
+        },
+      ],
+
+      'react/boolean-prop-naming': [
+        'error',
+        { propTypeNames: ['bool', 'mutuallyExclusiveTrueProps'] },
+      ],
+
+      'switch-case/newline-between-switch-case': [
+        'error',
+        'always',
+        { fallthrough: 'never' },
+      ],
+    },
   },
 ];
 
---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  tailwindFunctions: ['cn', 'clsx', 'twJoin', 'twMerge'],
+  trailingComma: 'es5',
+};
+
+export default config;
