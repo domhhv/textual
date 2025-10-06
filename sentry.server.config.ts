d9f047505d814ddb9d5fcfb8015b62ddcc9bd7fc
Dominik Hryshaiev <domhryshaiev@gmail.com>
2025-10-06 23:25:30 +0200

    chore: validate sentry dsn variables before initialiazing sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,14 +1,18 @@
 import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-  debug: false,
-  dsn: process.env.SENTRY_DSN,
-  enableLogs: true,
-  tracesSampleRate: 1,
-  integrations: [
-    Sentry.vercelAIIntegration({
-      recordInputs: true,
-      recordOutputs: true,
-    }),
-  ],
-});
+if (!process.env.SENTRY_DSN) {
+  console.warn('SENTRY_DSN is not set. Skipping Sentry initialization.');
+} else {
+  Sentry.init({
+    debug: false,
+    dsn: process.env.SENTRY_DSN,
+    enableLogs: true,
+    tracesSampleRate: 1,
+    integrations: [
+      Sentry.vercelAIIntegration({
+        recordInputs: true,
+        recordOutputs: true,
+      }),
+    ],
+  });
+}
--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -2,14 +2,20 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-  debug: false,
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  enableLogs: true,
-  integrations: [Sentry.replayIntegration()],
-  replaysOnErrorSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1,
-  tracesSampleRate: 1,
-});
+if (!process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  console.warn(
+    'NEXT_PUBLIC_SENTRY_DSN is not set. Skipping Sentry initialization.'
+  );
+} else {
+  Sentry.init({
+    debug: false,
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    enableLogs: true,
+    integrations: [Sentry.replayIntegration()],
+    replaysOnErrorSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    tracesSampleRate: 1,
+  });
+}
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
